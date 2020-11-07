d01ded748ff9ea92e33e0f65c16b43b4f7f3bc91
Raythonic <raythonic@gmail.com>
2020-11-07 14:58:39 -0800

    Added WTOuput object to encapsulate stdout, stderr, and exit code

--- src/watiba.py
+++ src/watiba.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3.8
-import subprocess
+from subprocess import Popen, PIPE, STDOUT
 import re
 import os
 
 
+class WTOutput(Exception):
+    def __init__(self):
+        self.stdout = []
+        self.stderr = []
+        self.exit_code = 0
+        self.cwd = "."
+
+    def set_stdout(self, stdout):
+        self.stdout = stdout
+
+    def set_stderr(self, stderr):
+        self.stderr = stderr
+
+    def set_exit_code(self, exit_code):
+        self.exit_code = exit_code
+
 class Watiba(Exception):
     def __init__(self):
-        self.out = []
+        self.out = WTOutput()
 
     def set_context(self):
-        for n,o in enumerate(self.out):
+        for n, o in enumerate(self.out.stdout):
             m = re.match(r'^_watiba_\((\S.*)\)$', o)
             if m:
                 os.chdir(m.group(1))
-                del self.out[n]
-
-
-    def getcwd(self):
-        return os.getcwd()
+                self.out.cwd = os.getcwd()
+                del self.out.stdout[n]
 
     # cmd - command string to execute
     # context = track or not track current dir
@@ -26,7 +39,14 @@ class Watiba(Exception):
     def bash(self, cmd, context=True):
         # Tack on this command to see what the current dir is after the user's command is executed
         ctx = ' && echo "_watiba_($(pwd))"' if context else ''
-        self.out = subprocess.check_output("{}{}".format(cmd, ctx), shell=True).decode('utf-8').split('\n')
+        p = Popen("{}{}".format(cmd, ctx),
+                  shell=True,
+                  stdout=PIPE,
+                  stderr=PIPE,
+                  close_fds=True)
+        self.out.set_exit_code(p.wait())
+        self.out.set_stdout(p.stdout.read().decode('utf-8').split('\n'))
+        self.out.set_stderr(p.stderr.read().decode('utf-8').split('\n'))
 
         # Are we supposed to track context?  Yes, then set Python's CWD to where the command took us
         if context:
