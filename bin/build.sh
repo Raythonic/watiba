c43892cfa1e119b121f0e1d077df1973c52ec4fa
Raythonic <raythonic@gmail.com>
2020-11-19 15:02:03 -0800

    Fix to build script.  It was producing too many "versions" at the top of the pre-compiler program

--- bin/build.sh
+++ bin/build.sh
@@ -16,14 +16,11 @@ fi
 mkdir tmp
 
 # Put version number in Watiba compiler code and build the dist package
-cp watiba/watiba-c.py tmp/.
-echo "#!/bin/python3" > tmp/temp.py
-echo "versions = [\"Watiba $ver\", \"Python 3.8\"]" >> tmp/temp.py
-cat watiba/watiba-c.py >> tmp/temp.py
-cp tmp/temp.py watiba/watiba-c.py
-cp watiba/watiba-c.py bin/.
+echo "#!/bin/python3" > bin/watiba-c.py
+echo "versions = [\"Watiba $ver\", \"Python 3.8\"]" >> bin/watiba-c.py
+cat watiba/watiba-c.py >> bin/watiba-c.py
 cp watiba/watiba.py tmp/.
-rm tmp/temp.py tmp/watiba-c.py
+rm tmp/temp.py
 
 # Build the dist package
 python3 setup.py sdist
