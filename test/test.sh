e1d46ba43423eef1641578f88a5537b59c43123e
Raythonic <raythonic@gmail.com>
2020-11-21 13:14:45 -0800

    Change to test script to making building optional

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd /home/rwalk/git/watiba
-echo "Building Watiba"
-bin/build.sh
+echo "Run build first?"
+read yn
+if [ "$yn" == "y" ]
+then
+  echo "Building Watiba"
+  bin/build.sh
+fi
 
 echo "Testing Watiba"
 bin/watiba-c.py test/examples.wt > tmp/watiba_test.py
