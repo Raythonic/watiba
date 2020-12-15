994abf85d91ba134a42199656ae7f0748063f534
Raythonic <raythonic@gmail.com>
2020-12-15 12:37:49 -0800

    Added runtime to tree_dump

--- bin/watiba-c.py
+++ bin/watiba-c.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-versions = ["Watiba 0.1.206", "Python 3.8"]
+versions = ["Watiba 0.1.207", "Python 3.8"]
 '''
 Watiba pre-complier.  Watiba commands are BASH embedded commands between backtick characters (i.e. `), like traditional Bash captures.
 
