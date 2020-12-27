ec1db1889346484f7ca7e39ec355119682a6c3d2
Raythonic <raythonic@gmail.com>
2020-12-27 12:36:06 -0800

    allow `cmd`@$host to be a host list or a string

--- watiba/wtspawncontroller.py
+++ watiba/wtspawncontroller.py
@@ -39,12 +39,15 @@ class WTSpawnController():
 
     # Start a thread belonging to the passed promise
     def start(self, promise, thread_callback, thread_args, host=None):
-        host_list = None
+        host_list = []
         if host:
             if type(host) == list:
                 host_list = host
             elif type(host) == str:
                 host_list = [host]
+        else:
+            host_list = self.args["hosts"]
+
         track_promise = True
         ex_count = self.args["expire"]
         loop_counter = 0
