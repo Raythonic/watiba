f8bbd734568ed1fab0aff910784fe74c2714c935
Ray Walker <raythonic@gmail.com>
2021-01-15 15:17:49 -0800

    Latest build for version 0.2.43

--- bin/watiba-c.py
+++ bin/watiba-c.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-versions = ["Watiba 0.2.42", "Python 3.8"]
+versions = ["Watiba 0.2.43", "Python 3.8"]
 '''
 Watiba pre-complier.  Watiba commands are BASH embedded commands between backtick characters (i.e. `), like traditional Bash captures.
 
