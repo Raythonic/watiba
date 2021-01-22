f1e7661bde428aff067950fe08266c486ea7ac41
Ray Walker <raythonic@gmail.com>
2021-01-22 08:27:06 -0800

    Polish up publish.sh

--- test/publish.sh
+++ test/publish.sh
@@ -4,36 +4,17 @@ test/build.sh
 ver=$(cat version.conf)
 echo "Build of ${ver} successful"
 
-branch=$(git branch | grep "\*" | awk '{print $2}')
 echo ""
-echo "I will now push ${ver} to github branch ${branch}"
-echo "Is this correct?"
-read yn
-if [ "$yn" != "y" ]
-then
-  echo "Terminating."
-  exit 0
-fi
-
-echo ""
-echo "Committing to git: Latest build for version ${ver}"
-git add .
-git commit -m "Latest build for version ${ver}"
-git push origin
-echo "Successfully pushed ${ver} to github branch ${branch}"
-
-echo ""
-echo "I will now push the Python package"
+echo "Pushing package ${ver}"
 test/push_package.sh
 
 echo ""
-echo "I will now uninstall the current py package and install the new one"
-test/install_package.sh
+echo "Installing package ${ver}"
 
 echo ""
-echo "I will now compile the examples test Watiba code"
+echo "Compiling examples and smoke test for ${ver}"
 test/compile.sh
 
 echo ""
-echo "Watiba published."
+echo "Watiba ${ver} published."
 
