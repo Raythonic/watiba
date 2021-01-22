95aa1d47164abb872d49b793b8187e768514e6ed
Ray Walker <raythonic@gmail.com>
2021-01-22 15:46:47 -0800

    Updating test cases

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-cd /home/rwalk/git/watiba
-bin/build.sh
-echo "Version $(cat version.conf) built."
-
-echo "Generating test Watiba examples in tmp/watiba_examples.py for build $(cat version.conf)"
-echo "Generating Watiba smoketest to tmp/watiba_smoke_test.py $(cat version.conf)"
+cd ~/git/watiba
 # Compile the examples
-bin/compile.sh
+if [ ! -d tmp ]
+then
+  mkdir tmp
+fi
+
+print("___________________________________________________________________________")
+print("Running smoke test against Watiba class functions")
+print("___________________________________________________________________________")
+test/smoke_test1.py
+
+echo "Compiling examples.wt and integrated_test1.wt"
+~/.local/bin/watiba-c examples/examples.wt > tmp/watiba_examples.py
+~/.local/bin/watiba-c tests/integrated_test1.wt > tmp/integrated_test1ntegrated_test1.py
+chmod +x tmp/watiba_*.py
+pushd tmp
+print("___________________________________________________________________________")
+print("Running examples")
+print("___________________________________________________________________________")
+./watiba_examples.py
+
+print("___________________________________________________________________________")
+print("Running Integration Test 1")
+print("___________________________________________________________________________")
+./integrated_test1.py
+popd
