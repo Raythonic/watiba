ea12ec5ff495554da964cb99cf1fd0d176dead97
Ray Walker <raythonic@gmail.com>
2021-01-24 19:21:48 -0800

    Improving prompts

--- bin/build.sh
+++ bin/build.sh
@@ -67,7 +67,7 @@ do
   if [ $chk_user_ver -ne 1 ]
   then
     echo "Incorrect format!  Must be nn.nn.nn"
-    echo "Re-enter new version number, or ENTER to keep the version number"
+    echo "Re-enter new version number, or ENTER to keep version number ${new_ver}"
     read resp
   else
       new_ver=${resp}
